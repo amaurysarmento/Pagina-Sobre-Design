65a0ba59c1eb94c8e71e26b895fce75a4b063e5a
Amaury Jr <121615990+amaurysarmento@users.noreply.github.com>
2023-07-25 15:58:01 -0300

    last update on 7/25/2023

--- sobreDesign.js
+++ sobreDesign.js
@@ -1,6 +1,13 @@
-let menuBar = document.querySelector('.menuBar');
+function OpenAndClose() {
+    let menuBar = document.querySelector('.menuBar');
+    
+    menuBar.addEventListener('click', function() {
+        const menuOpen = document.querySelector('nav');    
+        menuOpen.classList.toggle('menuList');
+        let NavbarAnimatio = document.querySelector('.container-menu');
+        NavbarAnimatio.CSSAnimation('animation');
+    })
+}
+OpenAndClose();
 
-menuBar.addEventListener('click', function() {
-    const menuOpen = document.querySelector('nav');    
-    menuOpen.classList.toggle('menuList');
-})
+
