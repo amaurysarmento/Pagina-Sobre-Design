83f45721ae0968d3393fc3ea845a55e73233f19d
Amaury Jr <121615990+amaurysarmento@users.noreply.github.com>
2023-07-28 09:04:02 -0300

    updated in 7/28/2023

--- sobreDesign.js
+++ sobreDesign.js
@@ -1,13 +1,29 @@
 function OpenAndClose() {
-    let menuBar = document.querySelector('.menuBar');
+    let menuBarClick = document.querySelector('.menuBar');
     
-    menuBar.addEventListener('click', function() {
+    menuBarClick.addEventListener('click', function() {
         const menuOpen = document.querySelector('nav');    
         menuOpen.classList.toggle('menuList');
-        let NavbarAnimatio = document.querySelector('.container-menu');
-        NavbarAnimatio.CSSAnimation('animation');
+        let NavbarAnimation = document.querySelector('.container-menu');
+        NavbarAnimation.CSSAnimation('animation');
     })
 }
-OpenAndClose();
 
+
+/**
+ * a classe .menuOut vai servir pra ser chamada pra retirar o menu da nav, fazendo a animação oposta que a do menu de entrada.
+ */
+
+
+// function NavOut() {
+//     let animationClose = document.querySelector('.menuBar');
+    
+//     animationClose.addEventListener('click', function() {
+//         const menuClose = document.querySelector('nav');    
+//         menuClose.classList.toggle('menuList');
+//         let NavbarOut = document.querySelector('.container-menu');
+//         NavbarOut.CSSAnimation('animationOut');
+//     })
+// }
+OpenAndClose();
 
